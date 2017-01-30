680f7f4d17734e8402366c9278e55efb184e44ab
Daniel Frick <danielfrick7@gmail.com>
2017-01-30 10:20:03 -0600

    CP-10 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
 /**
 * @desc object holds the current song
@@ -35,6 +35,12 @@
                  formats: ['mp3'],
                  preload: true
              });
+             
+             currentBuzzObject.bind('timeupdate', function() {
+                 $rootScope.$apply(function() {
+                     SongPlayer.currentTime = currentBuzzObject.getTime();
+                 });
+             });
 
              SongPlayer.currentSong = song;
          };
@@ -61,7 +67,12 @@
     };
 
 
-    SongPlayer.currentSong = null;
+        SongPlayer.currentSong = null;
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+        SongPlayer.currentTime = null;
 /**
 * @function Songplayer.play
 * @desc checks if current song playing
@@ -130,8 +141,18 @@
         };
             return SongPlayer;
         }
+/**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+*/
+    SongPlayer.setCurrentTime = function(time) {
+        if (currentBuzzObject) {
+            currentBuzzObject.setTime(time);
+        }
+    };
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
